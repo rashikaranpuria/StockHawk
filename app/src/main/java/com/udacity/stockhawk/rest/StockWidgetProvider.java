2b50ca52984d6d1b310beb65addcac25e289aabc
rashikaranpuria <2013165@iiitdmj.ac.in>
2017-01-05 13:51:28 +0530

    feat: widget not updating on change view

--- app/src/main/java/com/udacity/stockhawk/rest/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/rest/StockWidgetProvider.java
@@ -3,12 +3,14 @@ package com.udacity.stockhawk.rest;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.sync.QuoteSyncJob;
 import com.udacity.stockhawk.sync.StockWidgetListService;
 import com.udacity.stockhawk.ui.MainActivity;
 
@@ -32,8 +34,7 @@ public class StockWidgetProvider extends AppWidgetProvider {
                                 int appWidgetId) {
 
         Timber.d("in on updatind the widget ");
-
-        CharSequence widgetText = context.getString(R.string.app_name);
+                CharSequence widgetText = context.getString(R.string.app_name);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
         views.setTextViewText(R.id.app_widget_text, widgetText);
@@ -43,21 +44,24 @@ public class StockWidgetProvider extends AppWidgetProvider {
         views.setOnClickPendingIntent(R.id.widget,pendingIntent);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
     }
 
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+            super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
 //        context.startService(new Intent(context, StockWidgetService.class));
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-//        Timber.d("above on receive in stock widget provider"+intent.getAction());
-//
+        Timber.d("in on receive");
+
         super.onReceive(context, intent);
-//        if(QuoteSyncJob.ACTION_WIDGET_UPDATED.equals(intent.getAction())){
-//            Timber.d("on receive in stock widget provider");
-//            context.startService(new Intent(context, StockWidgetService.class));
-//        }
+        if(QuoteSyncJob.ACTION_WIDGET_UPDATED.equals(intent.getAction())){
+            Timber.d("on receive in stock widget provider");
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetManager.getAppWidgetIds(new ComponentName(context, StockWidgetProvider.class)), R.layout.stock_widget);
+        }
     }
 }
